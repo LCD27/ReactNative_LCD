782a168d4be21ea1137f3c30a12520626cd2f6f1
LCD27 <756355668@qq.com>
2019-03-04 10:08:07 +0800

    创建项目

---
+++ android/app/src/main/java/com/github_lcd/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github_lcd;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Github_LCD";
+    }
+}
